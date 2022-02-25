58188f9e213dd4d2e6c2c61a7e7873de78ec39f1
cuncis1st <cuncis1st@gmail.com>
2022-02-25 18:35:18 +0700

    ADD SOME codes

--- .github/actions/index.js
+++ .github/actions/index.js
@@ -4,12 +4,17 @@ const github = require('@actions/github')
 try {
     // throw new Error("Some error message")
     const name = core.getInput("who-to-greet")
+    core.setSecret(name)
     console.log(`Hello ${name}`)
 
     const time = new Date()
     core.setOutput("time", time.toDateString)
 
+    core.startGroup('Logging github object')
     console.log(JSON.stringify(github, null, "\t"))
+    core.endGroup()
+
+    core.exportVariable('HELLO', 'hello')
 } catch (error) {
     core.setFailed(error.message)
 }
